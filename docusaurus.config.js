df4eb9ae3eeebae2a0c471f390159a40b04c3772
Lucas Bica <a82451@ualg.pt>
2023-12-14 14:35:52 +0000

    Alteracao-Titulo-e-Subtitulo-Home

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Desnecessauro',
-  tagline: 'Jesse We Need To Cook',
+  title: 'Craques Do Ginásino',
+  tagline: 'Esteja estre os melhores no ginásio',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
